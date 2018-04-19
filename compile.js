fbb83f725467e0ae273ee55a909a5851a046dbda
胥涛 <xutao@lezhixing.com.cn>
2018-04-19 16:15:42 +0800

    在简陋

--- compile.js
+++ compile.js
@@ -2,9 +2,10 @@ function compile (template, data, el) {
   // 将template中需要绑定的属性识别出来。
   var attrs = template.match(/(?<=\{\{)(.*?)(?=\}\})/g)
   attrs.map((attr) => {
+    let res = data[attr]
     if (Dep.target) {
       Dep.target.addSub(function () {
-        var html = template.replace('{{'+attr+'}}', data[attr])
+        var html = template.replace('{{'+attr+'}}', res)
         el.innerHTML = html
       })
     }
